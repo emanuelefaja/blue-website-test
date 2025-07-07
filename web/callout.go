d53ce081ffd9bb83b8a293d7126b0fee9ba9e00b
Emanuele <107957269+emanuelefaja@users.noreply.github.com>
2025-07-07 10:05:56 -0500

    Updated callout

--- web/callout.go
+++ web/callout.go
@@ -199,7 +199,7 @@ func (r *CalloutRenderer) renderCallout(w util.BufWriter, source []byte, node as
 		// Clickable callout container with hover effects  
 		w.WriteString(fmt.Sprintf(`<a href="%s"%s class="block no-underline group">`, callout.To, target))
 		w.WriteString(`<div class="callout border border-gray-200 rounded-lg p-4 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 my-4 hover:border-brand-blue hover:bg-blue-50 dark:hover:border-blue-400 dark:hover:bg-blue-900/20 transition-all duration-200">`)
-		w.WriteString(`<div class="flex items-center gap-3">`)
+		w.WriteString(`<div class="flex items-start gap-3">`)
 		
 		// Icon container
 		w.WriteString(`<div class="w-10 h-10 bg-white dark:bg-gray-700 border border-gray-200 dark:border-gray-600 rounded-lg flex items-center justify-center flex-shrink-0">`)
@@ -218,7 +218,7 @@ func (r *CalloutRenderer) renderCallout(w util.BufWriter, source []byte, node as
 	} else {
 		// Non-clickable callout container
 		w.WriteString(`<div class="callout border border-gray-200 rounded-lg p-4 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 my-4">`)
-		w.WriteString(`<div class="flex items-center gap-3">`)
+		w.WriteString(`<div class="flex items-start gap-3">`)
 		
 		// Icon container
 		w.WriteString(`<div class="w-10 h-10 bg-white dark:bg-gray-700 border border-gray-200 dark:border-gray-600 rounded-lg flex items-center justify-center flex-shrink-0">`)
